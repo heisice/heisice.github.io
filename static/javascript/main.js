76426fdad22d27eeeda99022965f358d7bc20152
Ki-soo Kim <heisice@gmail.com>
2018-05-09 01:03:02 +0900

    인스타그램 피드 제거.

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -5,19 +5,4 @@ $(document).ready(function(){
 		return;
 	}
 
-
-	var feed = new Instafeed({
-		get: 'user',
-		userId: '741841',
-		clientId: 'd0ccf302923449508bd6e9eca76caa3b',
-		accessToken: '741841.4d295ec.3a915620392747edadc5c49ba9416a20',
-		sortBy: 'most-recent',
-		limit: 100,
-		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
-		success: function() {
-			$("#loading").remove();
-		}
-	});
-	feed.run();
-
 });
