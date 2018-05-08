a3b096c3f9bc8860baa9c4745d6759e5220352f1
Ki-soo Kim <heisice@gmail.com>
2018-05-09 00:58:58 +0900

    no message

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
 		userId: '741841',
 		clientId: 'd0ccf302923449508bd6e9eca76caa3b',
 		accessToken: '741841.4d295ec.3a915620392747edadc5c49ba9416a20',
+		sortBy: 'most-recent',
+		limit: 100,
 		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
 		success: function() {
 			$("#loading").remove();
